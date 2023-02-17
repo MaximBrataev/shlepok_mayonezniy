6df00d7c1b4f536358b7996ff9392b21cf346bb8
MaxBrat <privetovscky@gmail.com>
2023-02-17 14:31:44 +0300

    pre-final

--- Controller.js
+++ Controller.js
@@ -8,11 +8,12 @@ class Controller {
   async run() {
     this.model.readTopics();
     const rightAnswers = [
-      'Еще один шаг - и ты Чак Норис',
+      'Еще один шаг - и ты Чак Норрис',
       'Поздравляю, ты - Чак Норис',
       'Ты участвовал в битве экстрасенсов?',
       'Антон Долин, остановись!',
       'Признайся, ты стоял у нас за спиной при написании этого квиза',
+      'Для понижения самооценки переходи на следующий уровень',
     ];
     const wrongAnswers = [
       'У тебя еще есть шанс',
@@ -20,9 +21,9 @@ class Controller {
       'Поздравляю, ты больше не сын папы',
       'Ты уже понял, что кино - это не твое?',
       'Это 29 из 30',
+      'Ну ты понял',
     ];
-    const greetings = await this.view.greetings();
-    const whoYou = await this.view.userName();
+    await this.view.greetings();
     this.topics = await this.model.readTopics();
     const elTopic = await this.view.chooseTopics(this.topics);
     const quesObj = await this.model.readQuestions(elTopic);
@@ -36,12 +37,8 @@ class Controller {
       ) {
         indexTrue += 1;
         console.log(`\n${rightAnswers[indexTrue]}\n`);
-
-        // console.log(rightAnswers[Math.floor(Math.random() * 5)]);
         this.score += 1;
       } else {
-        // console.log(wrongAnswers[Math.floor(Math.random() * 5)]);
-
         indexFalse += 1;
         console.log(
           `\n${wrongAnswers[indexFalse]}. Правильный ответ: ${quesObj[i].answer
@@ -51,11 +48,7 @@ class Controller {
       }
       if (i === quesObj.length - 1) {
         // console.log(`Твой счёт ${this.score} из ${quesObj.length}`);
-        console.log(
-          `Unknown user, твой результат ${Math.floor(
-            (this.score / quesObj.length) * 100
-          )} `
-        );
+        console.log(`Unknown user, твой результат ${this.score} из 6.`);
       }
     }
   }
